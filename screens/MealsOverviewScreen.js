61c22c50a285303830190caf04ad43ede0a43b6c
Pantelis Papasavvas <pantelis.papasavvas@netcompany-intrasoft.com>
2023-11-02 16:43:25 +0200

    feat(mealItem): add images and styling

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,11 +1,35 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { MEALS } from '../data/dummy-data'
+import MealItem from '../components/MealItem';
 
 const MealsOverviewScreen = ({route}) => {
     const categoryId = route.params.categoryId;
+
+    const displayedMeals = MEALS.filter(item => {
+        return item.categoryIds.indexOf(categoryId) >= 0;
+    });
+
+    const renderMealItem = (itemData) => {
+        const mealItemProps = {
+            title: itemData.item.title,
+            imageUrl: itemData.item.imageUrl,
+            duration: itemData.item.duration,
+            complexity: itemData.item.complexity,
+            affordability: itemData.item.affordability
+        }
+        return (
+            <MealItem {...mealItemProps}/>
+        )
+    }
+
     return(
         <View style={styles.container}>
-            <Text>mealsOverview screen - {categoryId}</Text>
+        <FlatList 
+            data={displayedMeals}
+            keyExtractor={(item) =>item.id} 
+            renderItem={renderMealItem}
+        />
+
         </View>
     )
 }
