3e980b0aac33c9acc93c1c94a53c544f09585747
Pantelis Papasavvas <pantelis.papasavvas@netcompany-intrasoft.com>
2023-11-03 13:46:33 +0200

    feat(mealItem): set navigation option dynamically

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,24 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native';
-import { MEALS } from '../data/dummy-data'
+import {CATEGORIES, MEALS} from '../data/dummy-data'
 import MealItem from '../components/MealItem';
+import {useLayoutEffect} from "react";
 
-const MealsOverviewScreen = ({route}) => {
+const MealsOverviewScreen = ({route, navigation}) => {
     const categoryId = route.params.categoryId;
 
     const displayedMeals = MEALS.filter(item => {
         return item.categoryIds.indexOf(categoryId) >= 0;
     });
+
+    useLayoutEffect(() => {
+        const categoryTitle = CATEGORIES.find(
+          (category) => category.id === categoryId
+        ).title;
+
+        navigation.setOptions({
+            title: categoryTitle,
+        });
+    }, [categoryId, navigation]);
 
     const renderMealItem = (itemData) => {
         const mealItemProps = {
