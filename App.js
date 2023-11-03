d7c0cf711a6a1e6f5fb6a9d9b2f8707e6dd00071
Pantelis Papasavvas <pantelis.papasavvas@netcompany-intrasoft.com>
2023-11-03 13:19:26 +0200

    feat(mealItem): styling all screens with screenOptions property on stack navigator

--- App.js
+++ App.js
@@ -12,15 +12,16 @@ export default function App() {
     <>
       <StatusBar style='dark' />
       <NavigationContainer>
-        <Stack.Navigator>
+        <Stack.Navigator screenOptions={{
+          headerStyle: { backgroundColor: '#351401'},
+          headerTintColor: 'white',
+          contentStyle: {backgroundColor: '#3f2f25'}
+        }}>
           <Stack.Screen
             name='MealsCategories'
             component={CategoriesScreen}
             options={{
               title: 'All Categories',
-              headerStyle: { backgroundColor: '#351401'},
-              headerTintColor: 'white',
-              contentStyle: {backgroundColor: '#3f2f25'}
             }}
           />
           <Stack.Screen name='MealsOverview' component={MealsOverviewScreen}/>
