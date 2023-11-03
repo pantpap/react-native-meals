e0014063d5f091debc4db7bf13f1637219203e37
Pantelis Papasavvas <pantelis.papasavvas@netcompany-intrasoft.com>
2023-11-03 13:16:58 +0200

    feat(mealItem): styling screen with options property

--- App.js
+++ App.js
@@ -13,7 +13,16 @@ export default function App() {
       <StatusBar style='dark' />
       <NavigationContainer>
         <Stack.Navigator>
-          <Stack.Screen name='MealsCategories' component={CategoriesScreen}/>
+          <Stack.Screen
+            name='MealsCategories'
+            component={CategoriesScreen}
+            options={{
+              title: 'All Categories',
+              headerStyle: { backgroundColor: '#351401'},
+              headerTintColor: 'white',
+              contentStyle: {backgroundColor: '#3f2f25'}
+            }}
+          />
           <Stack.Screen name='MealsOverview' component={MealsOverviewScreen}/>
         </Stack.Navigator>
       </NavigationContainer>
